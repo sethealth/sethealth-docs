f00a99862ef6607c308760f20f5d01b93b0ff1c5
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2020-07-14 18:38:27 +0200

    Improve modules

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -9,17 +9,51 @@ const API = ({module}) => {
     .filter(fn => fn.flags.isExported && fn.kindString === 'Function')
     .map(fn => (
       <>
-        <h3>{renderFunction(fn)}</h3>
-        <p>{fn.comment?.shortText}</p>
+        <h3 class="code-h3">
+          <a
+            aria-hidden="true"
+            tabindex="-1"
+            class="anchor enhancedAnchor_node_modules-@docusaurus-theme-classic-src-theme-Heading-"
+            id={fn.name}
+          />
+          <code>{module}.{renderFunction(fn)}</code>
+          <a
+            aria-hidden="true"
+            tabindex="-1"
+            class="hash-link"
+            href={'#'+fn.name}
+            title="Direct link to heading"
+          >
+            #
+          </a>
+        </h3>
+        <p class="api-docs">
+          {fn.comment?.shortText}
+        </p>
+        <p class="api-docs">
+          {fn.comment?.text}
+        </p>
+        <br></br>
       </>
     ));
 }
 
 function renderFunction(fn) {
   const params = (fn.signatures[0].parameters)
-    ? fn.signatures[0].parameters.map(p => p.name).join(', ')
+    ? fn.signatures[0].parameters.map(p => {
+      const isOptional = p.flags.isOptional;
+      const typeConnector = isOptional ? '?:' : ':';
+      const type = p.type.type === 'union'
+        ? p.type.types.map(t => t.name).join(' | ')
+        : p.type.name;
+      return `\n  ${p.name}${typeConnector} ${type}`;
+    }).join(',')+'\n'
     : '';
-  return `${fn.name}(${params})`;
+  return [
+    `${fn.name}(`,
+    <span class="params">{params}</span>,
+    ')',
+  ];
 }
 
 export default API;
